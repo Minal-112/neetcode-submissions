11e2d5356b3ffa7efbe99edb9274e913fe2722cf
Minal-112 <minal.adsul11@gmail.com>
2026-04-29 11:41:16 +0530

    Bulk sync: 7 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    int nums[]={1,2,3,3};
+    public boolean hasDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+           for(int j=i+1; j<nums.length;j++){
+            if(nums[i]==nums[j])
+            {
+return true;
+            }
+        }
+        
+    }
+    return false;
+  }
+}
